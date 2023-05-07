d505c842ef4fbe77bb568156b55aae48a19edb0d
Asmir <asmir8717@gmail.com>
2023-05-07 21:25:14 +0900

    initial backend code and first half of github actions for backend deployment

---
+++ backend/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+  "net/http"
+  "fmt"
+  "math/rand"
+  "strconv"
+)
+
+func handleRoot(w http.ResponseWriter, req *http.Request) {
+  fmt.Println("request to root came")
+
+  w.WriteHeader(http.StatusOK)
+  w.Header().Set("Content-Type", "application/text")
+
+  w.Write([]byte("root"))
+}
+
+func handleHealthCheck(w http.ResponseWriter, req *http.Request) {
+  fmt.Println("request to healthcheck came")
+
+  w.WriteHeader(http.StatusOK)
+  w.Header().Set("Content-Type", "application/text")
+
+  w.Write([]byte("good"))
+}
+
+func handleRandomNumber(w http.ResponseWriter, req *http.Request) {
+  fmt.Println("request to random-number came")
+
+  w.WriteHeader(http.StatusOK)
+  w.Header().Set("Content-Type", "application/text")
+
+  randomNumber := rand.Intn(1000)
+  randomNumberText := strconv.Itoa(randomNumber)
+
+  w.Write([]byte(randomNumberText))
+}
+
+func main() {
+  http.HandleFunc("/api/", handleRoot)
+  http.HandleFunc("/api/healthcheck", handleHealthCheck)
+  http.HandleFunc("/api/handleHealthCheck", handleHealthCheck)
+
+  http.ListenAndServe(":80", nil)
+}
