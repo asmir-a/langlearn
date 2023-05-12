d820b2b91e0d24bf38f2ff4a4d5b5ecaee3f2af0
Asmir <asmir8717@gmail.com>
2023-05-12 16:29:34 +0900

    made chanages to the function pushing to database; specified types for pandas

--- backend/data/init/scripts/data-transforms/main.py
+++ backend/data/init/scripts/data-transforms/main.py
@@ -1,6 +1,9 @@
 #!./../../../venv/bin/python
 
+import sqlalchemy
 from sqlalchemy import create_engine
+from sqlalchemy.dialects import postgresql
+
 from merge_freq_and_word_info import get_full_word_info
 from simplify_definitions import simplify_definitions_column
 
@@ -10,6 +13,6 @@ def populate_table():
     words_pd = get_full_word_info()
     simplify_definitions_column(words_pd)
     engine = create_engine(CONNECTION_STRING)
-    words_pd.to_sql("words", engine)
+    words_pd.to_sql("words", engine, if_exists = "replace", dtype = {"word": sqlalchemy.TEXT(), "part_of_speech": sqlalchemy.TEXT(), "defs": postgresql.ARRAY(sqlalchemy.TEXT()), "freq_rank": sqlalchemy.INTEGER()})
 
 populate_table()
