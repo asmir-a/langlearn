c5fd426cdd6bc860b288dcaad747e4a38af1f73e
Asmir <asmir8717@gmail.com>
2023-07-15 18:29:36 +0900

    set up the health check endpoint for aws

--- backend/main.go
+++ backend/main.go
@@ -9,13 +9,19 @@ import (
 	wordgameRoutes "github.com/asmir-a/langlearn/backend/wordgame/routes"
 )
 
-
+func setUpHealthChecksForAws(mux *http.ServeMux) {
+	mux.Handle("/api/healthcheck",
+		http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+			w.Write([]byte("everything is okay"))
+		}))
+}
 
 func main() {
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
 
 	mux := &http.ServeMux{}
 
+	setUpHealthChecksForAws(mux)
 	dev.SetUpDevRoutes(mux)
 	authRoutes.SetUpAuthRoutes(mux)
 	wordgameRoutes.SetUpWordGameRoutes(mux)
