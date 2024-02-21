3465aff7b339fea350df2707be8f65c2c8d71bc3
Asmir <asmir8717@gmail.com>
2024-02-21 14:22:44 +0900

    added the admin user to iam

--- cloud/main.tf
+++ cloud/main.tf
@@ -19,11 +19,6 @@ provider "aws" {
     region = "ap-northeast-2"
 }
 
-resource "aws_instance" "app_server" {
-    ami = "ami-0f3a440bbcff3d043"
-    instance_type = "t2.micro"
-}
-
 resource "aws_ecr_repository" "ecr_repo" {
     name = "langlearn"
     image_tag_mutability = "MUTABLE"
@@ -34,3 +29,91 @@ resource "aws_ecr_repository" "ecr_repo" {
         app = "langlearn"
     }
 }
+
+resource "aws_iam_user" "langlearn_user_admin" {
+    name = "admin"
+}
+
+data "aws_iam_policy_document" "langlearn_policy_pc" {
+    statement {
+        effect = "Allow"
+        actions = ["iam:ChangePassword"]
+        resources = ["arn:aws:iam::*:user/&{aws:username}"]
+    }
+    statement {
+        effect = "Allow"
+        actions = ["iam:GetAccountPasswordPolicy"]
+        resources = ["*"]
+    }
+}
+
+data "aws_iam_policy_document" "langlearn_policy_admin" {
+    statement {
+        effect = "Allow"
+        actions = ["*"]
+        resources = ["*"]
+    }
+}
+
+resource "aws_iam_policy" "langlearn_policy_pc" {
+    name = "password-change-policy"
+    policy = data.aws_iam_policy_document.langlearn_policy_pc.json
+}
+
+resource "aws_iam_policy" "langlearn_policy_admin" {
+    name = "admin-policy"
+    policy = data.aws_iam_policy_document.langlearn_policy_admin.json
+}
+
+resource "aws_iam_policy_attachment" "langlearn_policy_admin_attachment" {
+    name = "admin-policy-attachment"
+    users = [aws_iam_user.langlearn_user_admin.name]
+    policy_arn = aws_iam_policy.langlearn_policy_admin.arn
+}
+
+resource "aws_iam_policy_attachment" "langlearn_policy_pc_attachment" {
+    name = "passowrd-change-policy-attachment"
+    users = [aws_iam_user.langlearn_user_admin.name]
+    policy_arn = aws_iam_policy.langlearn_policy_pc.arn
+}
+
+# resource "aws_ecs_task_definition" "langlearn_td" {
+#     family = "langlearn_td_family"
+#     requires_compatibilities = ["FARGATE"]
+#     network_mode = "awsvpc"
+#     cpu = 1024
+#     memory = 3072
+#     runtime_platform {
+#         cpu_architecture = "X86_64"
+#         operating_system_family = "LINUX"
+#     }
+#     container_definitions = {
+#     }
+# }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
